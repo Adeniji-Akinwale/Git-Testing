9e01aee386e7426be80a553875f92d4937174a12
Akinwale <Z3r0_D4y@localhost.localdomain>
2024-11-24 02:08:35 +0100

    My first commit in terminal

---
+++ app.js
@@ -0,0 +1,11 @@
+// console.log("hello form here")
+
+// function sumOfTwo(a,b){
+//     return `the sum of two numbers is: ${a+b}`
+// }
+
+// const sum= sumOfTwo(3,2)
+// console.log(sum)
+module.exports = function greet() {
+  console.log("Hello from file.js!");
+};
